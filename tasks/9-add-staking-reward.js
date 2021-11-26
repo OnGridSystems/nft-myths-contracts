a1cb41eaa0feb95227859126f1a6cf8a89646b32
Dmitry Suldin <d.suldin@ongrid.pro>
2021-11-26 16:16:46 +0300

    Add set reward task

---
+++ tasks/9-add-staking-reward.js
@@ -0,0 +1,24 @@
+const { task } = require('hardhat/config');
+
+task('9-add-staking-reward', 'set base reward per second').setAction(async(taskArgs, hre) => {
+  const ethers = hre.ethers;
+
+  const { execute } = hre.deployments;
+
+  const listAccounts = await ethers.provider.listAccounts();
+  const deployerAddress = listAccounts[0];
+
+  const price = 116057838661;
+
+  try {
+    await execute(
+      'NFTStaking',
+      { from: deployerAddress, log: true },
+      'setBaseRewardPerSecond',
+      price,
+    );
+    console.log(`BaseRewardPerSecond is set at ${price}`);
+  } catch (e) {
+    console.log(e);
+  }
+});
