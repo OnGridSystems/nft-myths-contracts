e2693c912b388ffd864aeeb006f86c735466c65d
Dmitry Suldin <d.suldin@ongrid.pro>
2021-12-16 09:45:00 +0300

    fix(staking) remove use initialize method from deploy script

--- deploy/staking-deploy.js
+++ deploy/staking-deploy.js
@@ -12,7 +12,6 @@ module.exports = async function({ ethers, getNamedAccounts, deployments }) {
     args: [nftlToken, heroesToken],
     proxy: {
       proxyContract: 'OpenZeppelinTransparentProxy',
-      methodName: 'initialize',
     },
     skipIfAlreadyDeployed: true,
   });
